f764ecbc7dd2b5027bb10368790a12f84c7ffb7d
BIVILLE Allan <biviall@ndlp.fr>
2021-05-27 15:32:12 +0200

    Jour3-2

---
+++ bd_telephone-v2.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 27 mai 2021 à 13:31
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `bd_telephone`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `marque`
+--
+
+DROP TABLE IF EXISTS `marque`;
+CREATE TABLE IF NOT EXISTS `marque` (
+  `id_marque` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_marque` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_marque`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `marque`
+--
+
+INSERT INTO `marque` (`id_marque`, `nom_marque`) VALUES
+(1, 'Apple'),
+(2, 'Huawei'),
+(3, 'Samsung');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `modele`
+--
+
+DROP TABLE IF EXISTS `modele`;
+CREATE TABLE IF NOT EXISTS `modele` (
+  `id_modele` int(11) NOT NULL AUTO_INCREMENT,
+  `nom_modele` varchar(20) NOT NULL,
+  `id_marque` int(11) NOT NULL,
+  PRIMARY KEY (`id_modele`),
+  KEY `id_marque` (`id_marque`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `modele`
+--
+
+INSERT INTO `modele` (`id_modele`, `nom_modele`, `id_marque`) VALUES
+(1, 'Iphone X', 1),
+(2, 'Iphone 11', 1),
+(3, 'Iphone 12', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `probleme`
+--
+
+DROP TABLE IF EXISTS `probleme`;
+CREATE TABLE IF NOT EXISTS `probleme` (
+  `id_probleme` int(3) NOT NULL AUTO_INCREMENT,
+  `nom_probleme` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_probleme`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `probleme`
+--
+
+INSERT INTO `probleme` (`id_probleme`, `nom_probleme`) VALUES
+(1, 'Ecran cassé');
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `modele`
+--
+ALTER TABLE `modele`
+  ADD CONSTRAINT `modele_ibfk_1` FOREIGN KEY (`id_marque`) REFERENCES `marque` (`id_marque`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- php/connexion_bd_telephone.php
+++ php/connexion_bd_telephone.php
@@ -1,8 +1,4 @@
 <?php
   $DataBase = mysqli_connect ( "localhost" , "root" , "" ) ;
-  mb_internal_encoding('UTF-8');
-  mysqli_query("SET NAMES 'utf8';");
-mysqli_query("SET CHARACTER SET 'utf8';");
   mysqli_select_db ( $DataBase, "bd_telephone" ) ;
-  
 ?>
